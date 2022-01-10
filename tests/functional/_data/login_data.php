3d98dadde1c471e0fb287490a833163218689d68
2423574112@qq.com <2423574112@qq.com>
2022-01-10 09:19:59 +0800

    codeception test data

---
+++ tests/functional/_data/login_data.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    [
+        'username' => 'test1',
+        'auth_key' => 'YHAjiUu45z6F8aDHHg3iRU20IUGeIroA',
+        // password_0
+        'password_hash' => '$2y$13$IZixmBr.x8vGs1RXWoK9D.48gsnpdJxU.08WrlJOVV3adV4WNN1q6',
+        'password_reset_token' => '08WrlJOVV3adV4WNN1q6',
+        'status' => '10',
+        'created_at' => '1641771648',
+        'updated_at' => '1641771648',
+        'email' => 'test1@qq.com',
+    ],
+    [
+        'username' => 'test2',
+        'auth_key' => 'NqbpESiIgJMNLlCrS9p3pUylsjcRs972',
+        // password_0
+        'password_hash' => '$2y$13$gXOXXQGAcPu6EGpaYjastuTrHr1aG2ArEIT08p9HSLiSjU6mgKTZO',
+        'password_reset_token' => 'Hr1aG2ArEIT08p9HSLiSjU6mgKTZO',
+        'status' => '10',
+        'created_at' => '1641771671',
+        'updated_at' => '1641771671',
+        'email' => 'test2@qq.com',
+    ],
+];
